ad0ea3cb91156138ec0a111c7c4d736bf587452e
Fernando Garcia Bernal <fegabe@gmail.com>
2020-05-30 19:35:08 +0200

    Adds relative domain

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ const SpaceShip = () => {
   const [model, setModel] = useState()
 
   useEffect(() => {
-    new GLTFLoader().load("/scene.gltf", setModel)
+    new GLTFLoader().load("./scene.gltf", setModel)
   })
 
   return model ? <primitive object={model.scene} /> : null
